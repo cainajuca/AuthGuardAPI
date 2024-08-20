a82ec1ba9e1ac549ff403653d0eeb3c0935646e7
Cainã Jucá <cainajuca@gmail.com>
2024-08-20 18:51:23 -0300

    Fix: JWT_SECRET was not using dotenv correctly

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,12 +6,14 @@ interface EnvConfig {
 	API_PORT: string;
 	API_URL: string;
 	DB_CONNECTION_STRING: string;
+	JWT_SECRET: string;
 }
 
 const requiredEnvVariables: (keyof EnvConfig)[] = [
 	'API_PORT',
 	'API_URL',
 	'DB_CONNECTION_STRING',
+	'JWT_SECRET',
 ];
 
 requiredEnvVariables.forEach((key) => {
@@ -24,4 +26,5 @@ export const config: EnvConfig = {
 	API_PORT: process.env.API_PORT!,
 	API_URL: process.env.API_URL!,
     DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING!,
+	JWT_SECRET: process.env.JWT_SECRET!,
 };
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,11 +1,12 @@
 import jwt from 'jsonwebtoken';
+import { config } from '../config/env';
 
 export interface JwtPayload {
   _id: string;
   username: string;
 }
 
-export const secretKey = process.env.JWT_SECRET;
+export const secretKey = config.JWT_SECRET;
 
 export function generateToken(payload: JwtPayload): string {
   return jwt.sign(payload, secretKey, { expiresIn: '1h' });

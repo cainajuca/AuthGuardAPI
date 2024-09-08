2ece9afbba02fcc44f831f5464b05760ebb04c6f
Cainã Jucá <cainajuca@gmail.com>
2024-09-08 19:08:28 -0300

    fix: let api run without Redis connection

--- src/4-infra/Cache/context.ts
+++ src/4-infra/Cache/context.ts
@@ -3,14 +3,18 @@ import { createClient, RedisClientType } from 'redis';
 let redisClient: RedisClientType | null = null;
 
 export const createRedisClient = async (): Promise<RedisClientType> => {
-    if (!redisClient) {
-        redisClient = createClient({
-            url: process.env.REDIS_URL,
-        });
-
-        await redisClient.connect();
-        console.log('Redis client connected.'); // remove when completed
+    try {
+        if (!redisClient) {
+            redisClient = createClient({
+                url: process.env.REDIS_URL,
+            });
+    
+            await redisClient.connect();
+            console.log('Redis client connected.'); // remove when completed
+        }
+    
+        return redisClient;
+    } catch (error) {
+        console.log('No caching available.'); // remove when completed
     }
-
-    return redisClient;
 };

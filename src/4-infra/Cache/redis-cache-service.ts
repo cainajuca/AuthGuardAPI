07de73ba5727b73d5a5a30515e23545fac6f6c87
Cainã Jucá <cainajuca@gmail.com>
2024-09-10 22:59:02 -0300

    feat: prevent API from crashing when Redis cache is unavailable

--- src/4-infra/Cache/redis-cache-service.ts
+++ src/4-infra/Cache/redis-cache-service.ts
@@ -6,15 +6,29 @@ export class RedisCacheService implements ICacheService {
 	constructor(private readonly redisClient: RedisClientType) { }
 
 	async get<T>(key: string): Promise<T | null> {
-		const data = await this.redisClient.get(key);
-		return data ? JSON.parse(data) : null;
+
+		try {
+			const data = await this.redisClient.get(key);
+			
+			return data ? JSON.parse(data) : null;
+		} catch (error) {
+			return null;
+		}
 	}
 
 	async set<T>(key: string, value: T, ttl?: number): Promise<void> {
-		await this.redisClient.set(key, JSON.stringify(value), { EX: ttl });
+
+		try {
+			await this.redisClient.set(key, JSON.stringify(value), { EX: ttl });
+		} catch (error) {
+		}
 	}
 
 	async delete(key: string): Promise<void> {
-		await this.redisClient.del(key);
+
+		try {
+			await this.redisClient.del(key);
+		} catch (error) {
+		}
 	}
 }

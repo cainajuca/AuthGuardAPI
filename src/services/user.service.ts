01e2d59e221fcf3c8ccf3ba6cf3d38bf18e0b12b
Cainã Jucá <cainajuca@gmail.com>
2024-09-22 15:17:34 -0300

    refact: handle generic errors in UserService

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -12,39 +12,46 @@ export class UserService {
 	constructor(private readonly userRepository: UserRepository) { }
 
 	async deleteUser(input: DeleteUserInput): Promise<DeleteUserOutput> {
-
-		const user = await this.userRepository.findById(input.id);
-
-		if(!user) {
-			return new DeleteUserOutput(false, null, 'User does not exist.');
+		try {
+			
+			const user = await this.userRepository.findById(input.id);
+			
+			if(!user) {
+				return new DeleteUserOutput(false, null, 'User does not exist.');
+			}
+			
+			await this.userRepository.delete(user.id);
+			
+			return new DeleteUserOutput(true, user);
+		} catch (error) {
+			return new DeleteUserOutput(false, null, 'An error occurred while trying to delete the user.');
 		}
-
-		await this.userRepository.delete(user.id);
-
-		return new DeleteUserOutput(true, user);
 	}
 
     async updateUser(input: UpdateUserInput): Promise<UpdateUserOutput> {
-
-		const user = await this.userRepository.findByUsername(input.username);
-
-		if(!user) {
-			return new UpdateUserOutput(false, null, 'User does not exist.');
+		try {
+			const user = await this.userRepository.findByUsername(input.username);
+			
+			if(!user) {
+				return new UpdateUserOutput(false, null, 'User does not exist.');
+			}
+			
+			if(input.password != input.confirmPassword) {
+				return new UpdateUserOutput(false, null, 'Password confirmation does not match the password.');
+			}
+			
+			const passwordHash = await hashPassword(input.password);
+			
+			user.name = input.name;
+			user.email = input.email;
+			user.username = input.username;
+			user.password = passwordHash;
+			
+			await this.userRepository.update(user);
+			
+			return new UpdateUserOutput(true, user);
+		} catch (error) {
+			return new UpdateUserOutput(false, null, 'An error occurred while trying to update the user.');
 		}
-
-		if(input.password != input.confirmPassword) {
-			return new UpdateUserOutput(false, null, 'Password confirmation does not match the password.');
-		}
-
-		const passwordHash = await hashPassword(input.password);
-		
-		user.name = input.name;
-		user.email = input.email;
-		user.username = input.username;
-		user.password = passwordHash;
-
-		await this.userRepository.update(user);
-
-		return new UpdateUserOutput(true, user);
 	}
 }
